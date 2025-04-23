474727bee0000d03a1fe98572084ea38d00f3ade
cmpdc <364850+cmpdc@users.noreply.github.com>
2025-04-23 15:34:54 -0400

    middleware auth tests

--- middleware/tests/auth.test.ts
+++ middleware/tests/auth.test.ts
@@ -31,14 +31,14 @@ describe("Auth Routes", () => {
 		expect(res.text.toLowerCase()).toContain("missing"); // or "error"
 	});
 
-	it(`POST /auth/refresh with missing body should return 400 — missing "refresh_token"`, async () => {
+	it(`POST /auth/refresh with missing body should return 400 with matched error message — missing "refresh_token"`, async () => {
 		const res = await request(app).post("/auth/refresh").send({}); // No refresh_token
 
 		expect(res.statusCode).toBe(400);
 		expect(res.text).toMatch(/missing authorization refresh_token/i);
 	});
 
-	it("POST /auth/refresh with mocked data should return 500 — invalid refresh_token", async () => {
+	it(`POST /auth/refresh with mocked data should return 500 with matched error message — invalid "refresh_token`, async () => {
 		const mockedSpotifyAccessFromLocalStorage = {
 			bearer: "BQChlO_ol-AGVTRcOHYURFCCylcWX7U27ADwQm7qFzI8iP6UwjGTmZZ9hJDLDyyujjb-lwr0EHWyzTwOPXK98YxjCXorwJKSZPdiBcHlhoxE0Aaz2OiiF_hSz427KQp_egSPBb0Mdt61594ePa4DHSpFAOCU2CzFYsGoMrPduTOeti9PmkuIT4e4LudY8ncXxCoURjsOifqNO4XSO6W3KWjL-dBesLnkBa5xuQ6K1I8NFdcnElTFf1V4-i515OvFxZM7zWSn",
 			expiration: "2025-04-23T20:17:34.536Z",
